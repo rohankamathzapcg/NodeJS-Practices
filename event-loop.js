956ce4ec532569bb4b48153640a34609f5325e08
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-15 16:46:16 +0530

    Event-Loop and ENV file

--- event-loop.js
+++ event-loop.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 2;
 
 setTimeout(() => console.log("Timer 1 Finished"), 0);
 setImmediate(() => console.log("Immediate 1 Finished"));
 
-fs.readFile("textfile.txt-", () => {
+//Event-loop
+fs.readFile("test-file.txt", () => {
   console.log("IO finished");
+
+  process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
 });
-s
+
 console.log("Hello from the top-level code");
